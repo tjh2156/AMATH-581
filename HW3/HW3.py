88487aa9dce2891de03c2f07b6b0ade9e54cae9d
Tyler <tjharper2020@gmail.com>
2024-10-29 12:58:13 -0700

    part C

--- HW3/HW3.py
+++ HW3/HW3.py
@@ -1,99 +1,184 @@
 import numpy as np
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
+from scipy import integrate
 import matplotlib.pyplot as plt
 
+def setupMatrices(dx, N):
+    #AssumeN anddxgiven
+        A= np.zeros((N,N))
+        for j in range(N-1):
+            A[j,j +1]= 1
+            A[j+ 1,j]=-1
+        A1=A /(2* dx)#Dirichletmatrices
+
+        A2=np.copy(A)
+        A2[0,0]=-4/3
+        A2[0,1]= 4/ 3
+        A2[N- 1,N- 1]=4 /3
+        A2[N- 1,N- 2]=-4 /3
+        A2=A2/ (2* dx)# Neumannmatrices
+
+        A3=np.copy(A)
+        A3[N- 1,0]= 1
+        A3[0,N- 1]=-1
+        A3=A3/ (2* dx)# PeriodicBC matrices
+
+        B = np.zeros((N, N))
+        for j in range(N):
+            B[j, j] =-2
+        for j in range(N- 1):
+            B[j, j + 1] = 1
+            B[j + 1, j] = 1
+        B1 = B / (dx**2) # Dirichlet matrices for B
+
+        B2 = np.copy(B)
+        B2[0, 0] =-2 / 3
+        B2[0, 1] = 2 / 3
+        B2[N- 1, N- 1] =-2 / 3
+        B2[N- 1, N- 2] = 2 / 3
+        B2 = B2 / (dx**2) # Neumann matrices for B
+
+        B3 = np.copy(B)
+        B3[N- 1, 0] = 1
+        B3[0, N- 1] =-1
+        B3 = B3 / (dx**2) # Periodic BC matrices for B
+
+        return A1, A2, A3, B1, B2, B3
+
 def partB():
-    def q(x):
-        return x**2
 
-    L = 4
+    L = 4 # domain size
     dx = .1
-    xspan = np.arange(-1*L, L+dx, dx)
-    epsilons = []
-    eigenfunctions = []
-    N = len(xspan)
+    x = np.arange(-L, L+dx, dx) # add boundary points
+    N = 79
 
-    A = np.matrix(np.zeros((N, N)))
-
-    for i in range(1,N - 1):
-        A[i,i] = 2/(dx**2) + q(xspan[i])
-        A[i,i + 1] = -1/(dx**2)
-        A[i,i - 1] = -1/(dx**2)
-
-    A[0,0] = 3/(dx**2)
-    A[0,1] = -4/(dx**2)
-    A[0,2] = 1/(dx**2)
-
-    A[-1,-1] = -3/(dx**2)
-    A[-1, -2] = 4/(dx**2)
-    A[-1, -3] = -1/(dx**2)
-
-    eigenvalues, eigenvectors = np.linalg.eigh(A)
+    A = np.zeros((N, N)) # Compute P matrix
+    for j in range(N):
+        A[j,j] = (-2- x[j+1]**2 * dx**2)/(-dx**2)
+        if j > 0:
+            A[j,j-1] = 1/(-dx**2)
+        if j < N - 1:
+            A[j,j+1] = 1/(-dx**2)
     
-    for i in range(len(eigenvalues)): #find first positive eigenvalue
-        if eigenvalues[i] > 0:
-            break
-
-    epsilons = eigenvalues[i:i+5] #get first 5 positive eigenvalues
-    for count in range(0, 5):
-        arrEigenvector = np.squeeze(np.asarray(eigenvectors[:,i+count]))
-        normedEigenfunction = eigenvectors[:,i+count]/np.sqrt(np.trapezoid(arrEigenvector**2, dx=dx))
-        if count == 1 or count == 2:
-            normedEigenfunction = -1*normedEigenfunction
-        plt.plot(xspan, normedEigenfunction)
-        eigenfunctions.append(abs(normedEigenfunction))
+    A[0,0] = 4/3/(-dx**2)
+    A[0,1] = -1/3/(-dx**2)
+    A[-1,-1] = 4/3/(-dx**2)
+    A[-1,-2] = -1/3/(-dx**2)
     
-    plt.legend(epsilons)
+    D, V = np.linalg.eig(A)
+
+    sorted_indices=np.argsort(np.abs(D))[::-1]
+    Dsort=D[sorted_indices]
+    Vsort=V[:,sorted_indices]
+    
+    D5 = np.abs(Dsort[N-5:N])
+    V5 = Vsort[:,N-5:N]
+
+    A3 = [] #eigenfunctions
+    A4 = D5 #eigenvalues
+
+    for k in range(5):
+        arrEigenvector = np.squeeze(np.asarray(V5[:,k]))
+
+        leftBoundary = 4/3 * arrEigenvector[0] - 1/3 * arrEigenvector[1]
+        rightBoundary = 4/3 * arrEigenvector[-1] - 1/3 * arrEigenvector[-2]
+        arrEigenvector = np.array([leftBoundary, *arrEigenvector, rightBoundary])
+
+        normedEigenfunction = arrEigenvector/np.sqrt(np.trapezoid(arrEigenvector**2, dx=dx))
+        plt.plot(x, normedEigenfunction)
+        A3.append(abs(normedEigenfunction))
+
+    A3 = np.array(A3).T
+    print(A3.shape)
+
+    plt.legend(np.asarray(D5, float))
     plt.xlabel('x')
     plt.ylabel('y')
     plt.title('Solution Plot')
     plt.grid(True)
     plt.show()
 
+    return A3, A4
 
-def partC():
-    def shoot(y, x, epsilon, gamma):
+
+def partC(L, dx, gammas):
+    def shoot(x, y, epsilon, gamma):
         return [y[1], y[0]*(gamma*abs(y[0]) + x**2 - epsilon)]
 
-    xspan = [-4, 4]  # x range
+    xspan = [-L, L+dx]  # x range
     # initial derivative value
     # step size for derivative adjustment
-    epsilon = 0
-    epsilons = []
-    eigenfunctions = []
-    gamma = 0.05
+    positiveGammaEpsilons = []
+    negativeGammaEpsilons = []
+    positiveGammaEigenfunctions = []
+    negativeGammaEigenfunctions = []
+    x = np.arange(xspan[0], xspan[1], dx)  # grid for odeint
+    for gamma in gammas:
+        epsilon = 0.1
+        for n in range(2):
+            amplitude = 1e-3
+            dAmplitude = 0.1
+            for _ in range(100):
+                dEpsilon = 0.2
+                for _ in range(1000):
+                    y0 = [amplitude, amplitude*np.sqrt(gamma + L**2 - epsilon)]  # initial condition
+                    
 
-    for n in range(2):
-        
-        dEpsilon = 0.1
-        for j in range(1000):
-            y0 = [1, np.sqrt(gamma + 16 - epsilon)]  # initial condition
-            x = np.arange(xspan[0], xspan[1] + .1, .1)  # grid for odeint
-            ysol = odeint(shoot, y0, x, (epsilon, gamma))  # solve ODE
-            
-            if abs(ysol[-1, 1] + np.sqrt(gamma*abs(ysol[-1,0]) + 16 - epsilon) * ysol[-1, 0]) < 1e-6:  # check convergence
-                break
-            
-            if (-1) ** (n+1) * (ysol[-1, 1] + (np.sqrt(gamma*abs(ysol[-1,0]) + 16 - epsilon) * ysol[-1,0])) < 0:
-                epsilon += dEpsilon
+                    sol = solve_ivp(shoot, xspan, y0, args=(epsilon, gamma), t_eval=x)  # solve ODE
+                    ysol = sol.y
+                    
+                    if abs(ysol[1, -1] + np.sqrt(L**2 - epsilon) * ysol[0, -1]) < 1e-6:  # check convergence
+                        break
+                    
+                    if (-1) ** (n+1) * (ysol[1, -1] + (np.sqrt(L**2 - epsilon) * ysol[0,-1])) < 0:
+                        epsilon += dEpsilon
+                    else:
+                        epsilon -= dEpsilon
+                        dEpsilon /= 2
+                area = integrate.simpson(np.squeeze(ysol[0,:]), x=x, dx=.1)
+                if area - 1 < 1e-6:
+                    break
+                if area < 1:
+                    amplitude += dAmplitude
+                elif area > 1:
+                    amplitude -= dAmplitude/2
+                    dAmplitude /= 2
+            normedEigenfunction = ysol[0, :]/np.sqrt(np.trapezoid(ysol[0,:]**2, x))
+            if gamma > 0:
+                positiveGammaEigenfunctions.append(abs(normedEigenfunction))
+                positiveGammaEpsilons.append(epsilon)
             else:
-                epsilon -= dEpsilon
-                dEpsilon /= 2
-        normedEigenfunction = ysol[:, 0]/np.sqrt(np.trapezoid(ysol[:,0]**2, x))
-        eigenfunctions.append(abs(normedEigenfunction))
-        epsilons.append(epsilon)
-        plt.plot(x, normedEigenfunction)
-        epsilon += 1
+                negativeGammaEigenfunctions.append(abs(normedEigenfunction))
+                negativeGammaEpsilons.append(epsilon)
+            plt.plot(x, abs(normedEigenfunction))
+            epsilon += .01
 
-    A1 = np.matrix(np.transpose(eigenfunctions))
-    A2 = np.array(epsilons)
+    A5 = np.matrix(np.transpose(positiveGammaEigenfunctions))
+    A6 = np.array(positiveGammaEpsilons)
 
-    plt.legend(epsilons)
+    A7 = np.matrix(np.transpose(negativeGammaEigenfunctions))
+    A8 = np.array(negativeGammaEpsilons)
+
+    plt.figure(1)
+    plt.plot(x, A5)
+    plt.legend(A6)
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.title('Solution Plot')
+    plt.title('Positive Gamma')
     plt.grid(True)
+
+    plt.figure(2)
+    plt.plot(x, A7)
+    plt.legend(A8)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Negative Gamma')
+    plt.grid(True)
+
     plt.show()
 
+    return A5, A6, A7, A8
 
-partB()
+
+# A3, A4 = partB()
+A5, A6, A7, A8 = partC(2, .1, [0.05, -0.05])
